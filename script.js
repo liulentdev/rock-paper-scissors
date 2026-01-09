e1c94a2ac8e71dc5cfcab7dfb1778ebaaf8d39e0
liulentdev <liulentdev@gmail.com>
2026-01-09 16:28:32 +0100

    Add playRound function
    Added feature to play one round and print out the result

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function getComputerChoice()
     //Math.random() vraca random float od 0 do 1(bez 1), pomnozis ga sa 3 i vraca do 2.7 max
     //Math.flor() broj koji dobije zaokruzi na manji ceo broj, dobije 2.7 zaokruzi na 2
     if(random == 0)
-        return "Rock!";
+        return "Rock";
     else if(random == 1)
-        return "Paper!";
+        return "Paper";
     else
-        return "Scissors!"
+        return "Scissors"
 }
 
 function getHumanChoice()
@@ -23,3 +23,46 @@ function getHumanChoice()
 }
 
 let humanScore = 0;
+let computerScore = 0;  
+
+function playRound(humanChoice, computerChoice)
+{
+    if(humanChoice == "Paper" && computerChoice == "Rock")
+    {
+        console.log("You win, Paper beats Rock!");
+        humanScore++;
+    }
+    else if(humanChoice == "Paper" && computerChoice == "Scissors")
+    {
+        console.log("You lose, Scissors beats Paper!");
+        computerScore++;
+    }
+    else if(humanChoice == "Rock" && computerChoice == "Paper")
+    {
+        console.log("You lose, Paper beats Rock!");
+        computerScore++;
+    }
+    else if(humanChoice == "Rock" && computerChoice == "Scissors")
+    {
+        console.log("You win, Rock beats Scissors!");
+        humanScore++;
+    }
+    else if(humanChoice == "Scissors" && computerChoice == "Rock")
+    {
+        console.log("You lose, Rock beats Scissors!");
+        computerScore++;
+    }
+    else if(humanChoice == "Scissors" && computerChoice == "Paper")
+    {
+        console.log("You win, Scissors beats Paper!");
+        humanScore++;
+    }
+    else if(humanChoice == computerChoice)
+        console.log("It's a tie!");
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
